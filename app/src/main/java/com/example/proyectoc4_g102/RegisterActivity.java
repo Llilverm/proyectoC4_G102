f0615ed7c1b15999b2999bdbb6614d056fc5e217
Llilverm <llilverm@gmail.com>
2022-11-10 19:28:17 -0500

    first commit

---
+++ app/src/main/java/com/example/proyectoc4_g102/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectoc4_g102;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
